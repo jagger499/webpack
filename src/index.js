26db3b596a792a8e564b6b71b6ad7652abfb1660
jhon garcia <jagg499@gmail.com>
2022-02-02 20:28:20 -0500

    prepare this project to pre-deploy on netlifu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from '@template/Template.js';
+// al añadir el minificador de css hay que eliminar
+// la parte de html donde se importa y agregarla acá
+import '@styles/main.css';
+import '@styles/vars.styl';
 
 (async function App() {
   const main = null || document.getElementById('main');
---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+// elemento dentro de node
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	// punto de entrada de la app
+	entry: './src/index.js',
+	// output que va a dar la salida dist -> distribution
+	output: {
+		// donde se va a guardar el proyecto
+		path: path.resolve(__dirname, 'dist'),
+		// nombre de donde se va a guardar el min
+		filename: '[name].[contenthash].js',
+		// change de  images
+    	assetModuleFilename: 'assets/images/[hash][ext][query]'
+	},
+	resolve: {
+		// extension que va a leer el archivo
+		extensions: ['.js'],
+		// añadir alias a las carpetas
+		// sirve para hacer imports por ejemplo desde
+		// import { util } from '@util/x_util.js'
+		alias: {
+			'@utils': path.resolve(__dirname, 'src/utils/'),
+			'@template': path.resolve(__dirname, 'src/templates/'),
+			'@styles': path.resolve(__dirname, 'src/styles/'),
+			'@images': path.resolve(__dirname, 'src/assets/images/'),
+		}
+	},
+	module: {
+		rules: [
+			{
+			test: /\.m?js$/,
+			// excluir node modules
+        	exclude: /node_modules/,
+			// se esta trabajando con babel-loader
+			use: {
+				loader: 'babel-loader'
+			  },
+			},
+			{
+				test: /\.css|\.styl$/i,
+				use: [MiniCssExtractPlugin.loader,'css-loader','stylus-loader']
+			},
+			{
+				// loader de imagenes de webpack
+				test: /\.png/,
+				type: 'asset/resource',
+			},
+			// loader para fuentes woff
+			{
+			  test: /\.(woff|woff2)$/,
+			  use: {
+				loader: 'url-loader',
+				options: {
+				  limit: 10000,
+				  mimetype: "application/font-woff",
+				  name: "[name].[contenthash].[ext]",
+				  outputPath: "./assets/fonts/",
+				  publicPath: "../assets/fonts/",
+				  esModule: false,
+				},
+			  },
+			}
+		],
+	},
+	// seccion de plugins
+	plugins: [
+		new HtmlWebpackPlugin({
+			// configuraciones del plugin
+			// insercion de los elementos
+			inject: true,
+			// pasar la direccion del template
+			template: './public/index.html',
+			// resultado del html
+			filename: './index.html',
+		}),
+		// add css to plugins
+		new MiniCssExtractPlugin({
+			filename: 'assets/[name].[contenthash].css'
+		}),
+		new CopyPlugin({
+			// elementos a copiar
+			patterns: [
+				{
+					// paste images to images in dist
+					from: path.resolve(__dirname, 'src', 'assets/images'),
+					to: 'assets/images',
+				}
+			]
+		}),
+		// inicializando las variables de entorno
+		new Dotenv(),
+		// inicializando plugin encargado de la limpieza de la carpeta dis
+		new CleanWebpackPlugin(),
+	],
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new CssMinimizerPlugin(),
+			new TerserPlugin(),
+		]
+	}
+}
